8f572b0bcc0887c67010a0166ad2dc8e6b60c2b5
maopos <mauricioposadamurillo@gmail.com>
2022-02-15 18:16:23 -0500

    Counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Box, Text, Flex } from "@chakra-ui/react";
+import Counter from "./Components/Counter";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Flex direction={"column"} alignItems={"center"}>
+      <Box
+        color={"white"}
+        bg={"darkcyan"}
+        w={"container.md"}
+        p={"1"}
+        mt={5}
+        borderRadius={10}
+        boxShadow={"2xl"}
+      >
+        <Text fontSize={"3xl"} textAlign="center">
+          Typescript
+        </Text>
+      </Box>
+      <Counter></Counter>
+    </Flex>
   );
 }
 
---
+++ src/Components/Counter.tsx
@@ -0,0 +1,95 @@
+import { Text, Box, Button, Stack, Flex } from "@chakra-ui/react";
+import React, { useState } from "react";
+
+const Counter = () => {
+  // Estado
+  const [counter, setCounter] = useState(0);
+
+  const incrementar = (numero: number = 1): void => {
+    setCounter(counter + numero);
+  };
+
+  return (
+    <Box
+      color={"white"}
+      bg={"darkcyan"}
+      w={{ sm: "container.md", md: "container.md" }}
+      p={"5"}
+      mt={2}
+      borderRadius={10}
+      boxShadow={"2xl"}
+    >
+      <Flex
+        direction="column"
+        alignItems="center"
+        justify="center"
+        h="full"
+        p={["0 4%", null, "0 20%"]}
+      >
+        <Text fontSize={"xl"} textAlign="center" fontWeight={"normal"}>
+          Counter
+        </Text>
+        <Text fontSize={"xl"} textAlign="center" fontWeight={"thin"}>
+          Valor: {counter}
+        </Text>
+        <Stack
+          direction={{ base: "column", md: "row" }}
+          spacing={4}
+          mt="20px"
+          w={["100%", null, "auto"]}
+        >
+          <Button
+            variant="outline"
+            size={"sm"}
+            textTransform={"uppercase"}
+            fontWeight={"light"}
+            color="white"
+            borderRadius={10}
+            letterSpacing={"1px"}
+            _hover={{
+              color: "black",
+              bg: "white",
+            }}
+            onClick={() => incrementar()}
+          >
+            + 1
+          </Button>
+          <Button
+            variant="outline"
+            size={"sm"}
+            textTransform={"uppercase"}
+            fontWeight={"light"}
+            color="white"
+            borderRadius={10}
+            letterSpacing={"1px"}
+            _hover={{
+              color: "black",
+              bg: "white",
+            }}
+            onClick={() => incrementar(2)}
+          >
+            + 2
+          </Button>
+          <Button
+            variant="outline"
+            size={"sm"}
+            textTransform={"uppercase"}
+            fontWeight={"light"}
+            color="white"
+            borderRadius={10}
+            letterSpacing={"1px"}
+            _hover={{
+              color: "black",
+              bg: "white",
+            }}
+            onClick={() => setCounter(0)}
+          >
+            Reset
+          </Button>
+        </Stack>
+      </Flex>
+    </Box>
+  );
+};
+
+export default Counter;
